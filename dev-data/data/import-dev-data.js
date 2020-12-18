fe0faf2d4436f96ccc61f569b6823f88893f3e0d
Andrii Matviichuk <matviichuk.andr@gmail.com>
2020-12-18 13:23:36 +0100

    API improvements

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './.env' });
+
+const DB = process.env.DATABASE_CONNECTION.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection was successfull'))
+  .catch((err) => console.log('Failed to connect to database: ', err));
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data succesfully loaded');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data succesfully deleted');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
