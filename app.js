48588bb6e7c23e55f5283b1ff428d577a83b0c87
Andrii Matviichuk <matviichuk.andr@gmail.com>
2020-12-25 19:02:28 +0100

    Security features

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -11,19 +14,46 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// Middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Global middleware
+//HTTP security header
+app.use(helmet());
+
+// Limit requests
 const limiter = rateLimit({
-  max: 3,
+  max: 100,
   window: 60 * 60 * 1000,
   message: 'Too many requests from this IP, try again in an hour!',
 });
 app.use('/api', limiter); // affect only routes which start from /api
 
-app.use(express.json());
+// Body parser, data from req.body with limit of 10kb
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against Cross Site Scripting Attacks (XSS)
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // Mounting routers
