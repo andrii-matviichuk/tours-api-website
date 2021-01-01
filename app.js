aee2806874e35a15950a77c7bb1ca752e3107b3e
Andrii Matviichuk <matviichuk.andr@gmail.com>
2021-01-01 15:39:02 +0100

    Rendering pug templates

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -62,10 +63,8 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
-  res.status(200).render('base');
-});
 // Mounting routers
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
